290a878bb7c75999e34473527b9ca2b727451c72
Anand Balivada <46479869+Poincare057@users.noreply.github.com>
2021-01-12 22:28:42 +0530

    Add files via upload

--- nbody.py
+++ nbody.py
@@ -1,14 +1,21 @@
-# n-body code
+##CODE TO NUMERICALLY INTEGRATE THE N-BODY PROBLEM
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# masses
+#Number of bodies
 n = 3
+
+#UI Stuff
+coord = ["x", "y", "z"]
+
+# masses
 m = np.zeros(n)
 for i in range(n):
-    m[i] = float(input())
-    
+    m[i] = float(input("Enter mass of " + str(i) + "-th body: "))
+print()
+
 #constants
 G = 6.67*10**-4
 
@@ -16,13 +23,13 @@ G = 6.67*10**-4
 T = 100000
 h = 0.01
 
-r = np.zeros((T, n, 3))
-
 # Specifying structure of differential equations
-def norm(l):
-    return (l[0]**2 + l[1]**2 + l[2]**2)**0.5
+r = np.zeros((T, n, 3))     #Positions of all the n bodies in 3D space from time [0, T*h]
 
-def f(r):
+def norm(l):                                    #Magnitude of a vector (L2 norm)
+    return (l[0]**2 + l[1]**2 + l[2]**2)**0.5                       
+
+def f(r):                   #In the differential equation d^2 r_i/dt^2 = f_i(r), where r = (r_1, ..., r_n), and r_i = (x_i, y_i, z_i), f(r) returns all the f_i
     f = np.zeros((n,3))
     for i in range(n):
         for j in range(n):
@@ -34,12 +41,13 @@ def f(r):
 
 #Numerical Integration of the n-body system
 
-#Initial Conditions
-eps = 0.1
+eps = 0.1   #Initial condition for derivative
+
 for i in range(n):
     for j in range(3):
-        r[0][i][j] = float(input())
+        r[0][i][j] = float(input(coord[j] + " coordinate of " + str(i) + "-th body: "))
         r[1][i][j] = r[0][i][j] + eps*np.random.random()
+    print()
         
 for t in range(2, T):
     r[t] = 2*r[t-1] - r[t-2] + (h**2)*f(r[t-2])
@@ -80,6 +88,24 @@ def config_plot2D(code):
         rplot2[t] = r[t][code[2]][code[3]]
     plt.plot(rplot, rplot2)
     plt.show()
+
+#plots 3D trajectories of all the n bodies
+def plot_all():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection = '3d')
+    for i in range(n):
+        rplot = np.zeros((T))
+        for t in range(T):
+            rplot[t] = r[t][i][0]
+        rplot2 = np.zeros((T))
+        for t in range(T):
+            rplot2[t] = r[t][i][1]
+        rplot3 = np.zeros((T))
+        for t in range(T):
+            rplot3[t] = r[t][i][2]
+        ax.plot(rplot, rplot2, rplot3)
+    plt.show()
+        
     
 
 
